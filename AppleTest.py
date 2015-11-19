975fe38afbc7a520e9df4cfdab6e96148d2cab45
bturcott <bradford727@gmail.com>
2015-11-18 22:08:11 -0800

    Added connection warning

--- AppleTest.py
+++ AppleTest.py
@@ -10,7 +10,6 @@ from PIL import Image, ImageTk
 #initialize socket connection
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
 #Creates the window and labels it
 top = Tkinter.Tk()
 top.wm_title("Apple Tester")
@@ -18,7 +17,7 @@ top.wm_title("Apple Tester")
 #Sets the apple background image
 bkg_img = Image.open('/home/bturcott/Pictures/apple_small.png')
 tkimage = ImageTk.PhotoImage(bkg_img)
-Tkinter.Label(top,image = tkimage).grid(row=0, column=3, rowspan=3, sticky=W+E+N+S, padx=5, pady=5)
+Tkinter.Label(top,image = tkimage).grid(row=0, column=3, rowspan=4, sticky=W+E+N+S, padx=5, pady=5)
 
 #Label for IP Address
 label1 = Label(top, text = "Enter Atlas IP Address:")
@@ -39,13 +38,16 @@ entry2.grid(row=1, column=1, sticky=N)
 
 def atlasConnect():
 	host = entry1.get()
-	client_socket.connect((host, 8000))
-	tkMessageBox.showinfo("Atlas Connection Status", "You are connected.")
+	if host != '':		
+		client_socket.connect((host, 8000))
+		connected = True		
+		tkMessageBox.showinfo("Atlas Connection Status", "You are connected.")
+	else:	
+		tkMessageBox.showwarning("Connection Warning", "You must enter an IP address to connect")
 
 def readString():
 	data = client_socket.recv(512)
 	tkMessageBox.showinfo("Incoming Message", data) 
-		
 
 def sendString():
 	totalsent = 0
@@ -58,16 +60,16 @@ def sendString():
 		totalsent = totalsent + sent
 	tkMessageBox.showinfo("Message Status", "Your message has been sent.") 
 
+#Button for sending data
+button1 = Tkinter.Button(top,text = "Send", command=sendString)
+button1.grid(row=3,column=1, sticky=E)
+
 #Button for reading data
 button1 = Tkinter.Button(top,text = "Read", command=readString)
-button1.grid(row=2,column=1, sticky=N+E)
+button1.grid(row=3,column=1, sticky=W)
 
 #Button for connecting to the Atlas
 button1 = Tkinter.Button(top,text = "Connect", command=atlasConnect)
-button1.grid(row=2,column=1, sticky=N+W)
+button1.grid(row=2,column=1, columnspan = 2, sticky=N)
 
-#labelframe.pack(fill="both", expand="yes")
-
-#winlabel.pack()
-#button1.pack()
 top.mainloop()
